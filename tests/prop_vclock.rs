bf090cd10e24992f3e78fd71696cf8be60f69ab5
Vitor Enes <vitorenesduarte@gmail.com>
2019-03-09 20:33:10 +0000

    Add union property test

--- tests/prop_vclock.rs
+++ tests/prop_vclock.rs
@@ -16,3 +16,15 @@ fn add_dot(dot: Dot<u64>, vclock: VClock<u64>) -> bool {
     vclock.add_dot(&dot);
     vclock.is_element(&dot)
 }
+
+#[quickcheck]
+fn union(vclock_a: VClock<u64>, vclock_b: VClock<u64>) -> bool {
+    let mut vclock_a = vclock_a.clone();
+    vclock_a.union(&vclock_b);
+
+    // prop: after merging b into a, all events in b are events in a
+    vclock_b.into_iter().all(|(actor, seq)| {
+        let dot = Dot::new(&actor, seq);
+        vclock_a.is_element(&dot)
+    })
+}
